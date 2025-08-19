32641223f470c25004a07f268403059ac694f385
Guilherme Xavier Zanetti <zanettIno>
2025-08-19 11:04:03 -0300

    config/todev-files-added

--- candi-app/src/screens/profile/settings.tsx
+++ candi-app/src/screens/profile/settings.tsx
@@ -2,13 +2,11 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 
-export default function Index() {
+export default function Settings() {
   return (
     <PaperProvider>
       <View>
-        <Text>
-            Oioi Candi!! CHZ
-        </Text>
+        
       </View>
     </PaperProvider>
   );
