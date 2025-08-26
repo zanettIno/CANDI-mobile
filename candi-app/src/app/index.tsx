fec6302e213bccee36caa6a6c8bb43c9f488db80
Guilherme Xavier Zanetti <zanettIno>
2025-08-25 22:11:41 -0300

    component: start of the backgrounds, need to create the home's index.tsx

--- candi-app/src/app/index.tsx
+++ candi-app/src/app/index.tsx
@@ -3,15 +3,17 @@ import { View, Text } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 import { AppTheme } from '../theme';
 import * as Kadwa from '@expo-google-fonts/kadwa';
+import LoginSignupBackground from '@/components/LoginSignupBackground';
 
 export default function Index() {
   return (
     <PaperProvider theme={AppTheme}>
-      <View>
+      <LoginSignupBackground>
         <Text style={{ color: AppTheme.colors.textColor, fontSize: 20, margin: 20,  fontFamily: AppTheme.fonts.bodyMedium.fontFamily}} >
             Oioi Candi!! CHZ
         </Text>
-      </View>
+      </LoginSignupBackground>
     </PaperProvider>
   );
 }
+
