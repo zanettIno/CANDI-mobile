2bcb8de8381bd890952e515df9ef5ef0a5a98ddf
Carolina Pichelli <carolinapichellisouza@gmail.com>
2025-08-26 08:55:39 -0300

    feat: start of development of the carousel component

--- candi-app/src/app/index.tsx
+++ candi-app/src/app/index.tsx
@@ -2,15 +2,33 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 import { AppTheme } from '../theme';
-import * as Kadwa from '@expo-google-fonts/kadwa';
+import Carousel, { CarouselItem } from '../components/Carousel/carousel';
+
+const demoData: CarouselItem[] = [
+  { title: 'Slide 1', image: 'https://picsum.photos/600/400?1' },
+  { title: 'Slide 2', image: 'https://picsum.photos/600/400?2' },
+  { title: 'Slide 3', image: 'https://picsum.photos/600/400?3' },
+];
 
 export default function Index() {
   return (
     <PaperProvider theme={AppTheme}>
-      <View>
-        <Text style={{ color: AppTheme.colors.textColor, fontSize: 20, margin: 20,  fontFamily: AppTheme.fonts.bodyMedium.fontFamily}} >
-            Oioi Candi!! CHZ
+      <View style={{ flex: 1, paddingTop: 50, backgroundColor: AppTheme.colors.background }}>
+        <Text
+          style={{
+            color: AppTheme.colors.textColor,
+            fontSize: 20,
+            margin: 20,
+            fontFamily: AppTheme.fonts.bodyMedium.fontFamily,
+          }}
+        >
+          Oioi Candi!! CHZ
         </Text>
+
+        <Carousel
+          data={demoData}
+          onItemPress={(item) => console.log('Clicou em:', item)}
+        />
       </View>
     </PaperProvider>
   );
