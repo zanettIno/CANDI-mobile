c7a340c56913f51fe5c14d98f002f9da958a7fd3
Carolina <carolinapichellisouza@gmail.com>
2025-09-14 20:48:27 -0300

    feat(diary): create initial diary screen

--- candi-app/src/components/NewPassageFAB/index.tsx
+++ candi-app/src/components/NewPassageFAB/index.tsx
@@ -1,32 +1,32 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
 import { FAB } from 'react-native-paper';
-import { AppTheme } from '../../theme'; 
+import { AppTheme } from '../../theme';
 
 type NewPassageFABProps = {
-  onPress: () => void; 
+  onPress: () => void;
 };
 
-const NewPassageFAB: React.FC<NewPassageFABProps> = ({ onPress }) => {
-  return (
-    <FAB
-      icon="pencil"
-      style={styles.fab}
-      onPress={onPress}
-      color={AppTheme.colors.cardBackground} 
-    />
-  );
-};
+const NewPassageFAB: React.FC<NewPassageFABProps> = ({ onPress }) => (
+  <FAB
+    icon="pencil"
+    style={styles.fab}
+    onPress={onPress}
+    color={AppTheme.colors.cardBackground}
+    animated
+    accessibilityLabel="Adicionar novo registro"
+  />
+);
 
 const styles = StyleSheet.create({
   fab: {
     position: 'absolute',
-    zIndex: 1,
+    zIndex: 100,
     margin: 24,
     marginBottom: 64,
     right: 0,
     bottom: 0,
-    backgroundColor: AppTheme.colors.primary, 
+    backgroundColor: AppTheme.colors.primary,
   },
 });
 
